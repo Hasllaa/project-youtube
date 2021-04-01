a73ca7c45ebb693adcd706de3ad9786d92215e4f
Hasllaa <money26900@gmail.com>
2021-04-01 12:08:07 +0900

    youtube first

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
+import './app.css';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBFBlmIM4eqrWtJDTTvtwRzYEvwBl4VlJs", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
